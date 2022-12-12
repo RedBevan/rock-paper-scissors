a0884b46e5852f756c768703ac57e67f2fbbea84
Joe <jrbevan537@gmail.com>
2022-12-12 19:46:00 +0100

    Create game() function that runs through 5 rounds

--- script.js
+++ script.js
@@ -17,35 +17,51 @@ function getComputerChoice() {
 const computerChoice = getComputerChoice();
 console.log(computerChoice);
 
-const userInput = prompt('Choose your weapon!', 'Rock'); //Get user choice from a prompt
-const userChoice = userInput.toLowerCase(); //Convert user choice to lower case
-console.log(userChoice);
 
-function playRound() {
+
+function playRound() { /* This function plays single round of RPS */
+  const userInput = prompt('Rock, paper or scissors?', ''); //Get user choice from a prompt
+  const userChoice = userInput.toLowerCase(); //Convert user choice to lower case
+  console.log(userChoice);
   if (userChoice === computerChoice) {
+    console.log("It's a draw!");
         return "It's a draw!";
     } else if (userChoice === "rock") {
       if (computerChoice === "paper") {
+        console.log('You lose, ' + computerChoice + ' beats ' + userChoice + '!');
         return 'You lose, ' + computerChoice + ' beats ' + userChoice + '!';
       } else if (computerChoice === "scissors") {
+        console.log('You win, ' + userChoice + ' beats ' + computerChoice + '!');
         return 'You win, ' + userChoice + ' beats ' + computerChoice + '!';
       }
       } else if (userChoice === "paper") {
         if (computerChoice === "rock") {
+          console.log('You win, ' + userChoice + ' beats ' + computerChoice + '!');
           return 'You win, ' + userChoice + ' beats ' + computerChoice + '!';
         } else if (computerChoice === "scissors") {
+          console.log('You lose, ' + computerChoice + ' beats ' + userChoice + '!');
           return 'You lose, ' + computerChoice + ' beats ' + userChoice + '!';
         }
       } else if (userChoice === "scissors") {
         if (computerChoice === "rock") {
+          console.log('You lose, ' + computerChoice + ' beats ' + userChoice + '!');
           return 'You lose, ' + computerChoice + ' beats ' + userChoice + '!';
         } else if (computerChoice === "paper") {
+          console.log('You win, ' + userChoice + ' beats ' + computerChoice + '!');
       } return 'You win, ' + userChoice + ' beats ' + computerChoice + '!';
       }
       
 }
 
-console.log(playRound());
+function game() {
+  for (let i = 0; i < 5; i++) {
+    if (i < 5) {
+      playRound();
+    }
+ }
+}
+
+game();
 
   /* Write function that plays single round of Rock Paper Scissors.
   Function should take two parameters - the playerChoice and 
